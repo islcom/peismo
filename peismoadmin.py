8f6f3cbcd7bb834eb067bd4fae3206335bfd9c6a
Colin Love <thecolinlove@gmail.com>
2024-01-02 06:14:16 +0000

    added automated folder creation

--- peismoadmin.py
+++ peismoadmin.py
@@ -1,6 +1,14 @@
 from ftplib import FTP
 import os, subprocess, sys
 
+
+if not os.path.exists("data"):
+    os.makedirs("data")
+if not os.path.exists("mseed"):
+    os.makedirs("mseed")
+if not os.path.exists("uploaded"):
+    os.makedirs("uploaded")
+
 running = False
 ss = str(subprocess.check_output(["ps", "-e"])).split("\n")
 for s in ss:
